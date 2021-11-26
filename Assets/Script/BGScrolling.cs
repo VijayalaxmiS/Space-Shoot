4effb5d3f03835489f4d115df1a4484601040d3b
VijayalaxmiS <vijayalaxmirms@gmail.com>
2021-11-26 18:10:05 +0530

    BGScroll

---
+++ Assets/Script/BGScrolling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScrolling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = -2f;
+    // Update is called once per frame 
+    void Update()
+    {
+        transform.Translate(0f, speed * Time.deltaTime, 0f);
+        if (transform.position.y <= -20)
+        {
+            transform.Translate(0f, 40f, 0f);
+        }
+    }
+}
